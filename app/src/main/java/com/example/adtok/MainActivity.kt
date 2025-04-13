fcee1fc97bc7bb0ea7c7e9ff1f80a5e20de617b4
BlastGhost <47760486+BlastGhost@users.noreply.github.com>
2025-04-13 11:47:14 +0200

    [Main Activity] Create main activity

--- app/src/main/java/com/example/adtok/MainActivity.kt
+++ app/src/main/java/com/example/adtok/MainActivity.kt
@@ -4,77 +4,70 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.adtok.component.ActionsBar
+import com.example.adtok.component.AdInfo
+import com.example.adtok.component.AdTokUI
 import com.example.adtok.component.BottomNavBar
-import com.example.adtok.component.Title
+import com.example.adtok.component.Swiper
 import com.example.adtok.ui.theme.AdTokTheme
-import com.example.adtok.ui.theme.Modifiers
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AdTokTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
+            Main()
+        }
+    }
+}
+
+
+@Composable
+private fun Main(modifier: Modifier = Modifier) {
+    val contents = IntRange(1, 10).toList()
+
+    AdTokTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {  },
+            bottomBar = { BottomNavBar() },
+            floatingActionButton = { ActionsBar() },
+        ) { innerPadding ->
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                Swiper(
+                    contents,
+                    padding = innerPadding.calculateBottomPadding() + innerPadding.calculateTopPadding()
+                ) { content ->
+                    Text(
+                        text = "$content",
+                        color = Color.White
+                    )
                     Box(
                         Modifier
-                            .padding(innerPadding)
-                            .fillMaxSize(),
+                            .fillMaxWidth()
+                            .align(Alignment.TopCenter)
                     ) {
-                        Box(
-                            Modifier
-                                .fillMaxWidth()
-                                .height(100.dp)
-                                .background(
-                                    brush = Brush.verticalGradient(
-                                        colors = listOf(
-                                            Color.Black,
-                                            Color.Transparent,
-                                        ),
-                                        startY = 50f,
-                                    ),
-                                )
-                                .padding(Modifiers.paddingLarge)
-                                .align(Alignment.TopStart)
-                        ) {
-                            Title(
-                                "Ad Name Title",
-                                "Brand Name "
-                            )
-                        }
-
-                        Column(
-                            Modifier
-                                .fillMaxWidth()
-                                .align(Alignment.BottomCenter),
-                            horizontalAlignment = Alignment.End
-                        ) {
-                            Box(
-                                Modifier.padding(bottom = Modifiers.paddingLarge, end = Modifiers.paddingLarge)
-                            ) {
-                                ActionsBar()
-                            }
-                            BottomNavBar()
-                        }
+                        AdInfo(
+                            "Ad : $content",
+                            "Brand Name "
+                        )
                     }
                 }
             }
@@ -85,52 +78,6 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AdTokTheme {
-        Scaffold(
-            modifier = Modifier.fillMaxSize()
-        ) { innerPadding ->
-            Box(
-                Modifier
-                    .padding(innerPadding)
-                    .fillMaxSize(),
-            ) {
-                Box(
-                    Modifier
-                        .fillMaxWidth()
-                        .height(100.dp)
-                        .background(
-                            brush = Brush.verticalGradient(
-                                colors = listOf(
-                                    Color.Black,
-                                    Color.Transparent,
-                                ),
-                                startY = 50f,
-                            ),
-                        )
-                        .padding(Modifiers.paddingLarge)
-                        .align(Alignment.TopStart)
-                ) {
-                    Title(
-                        "Ad Name Title",
-                        "Brand Name "
-                    )
-                }
-
-                Column(
-                    Modifier
-                        .fillMaxWidth()
-                        .align(Alignment.BottomCenter),
-                    horizontalAlignment = Alignment.End
-                ) {
-                    Box(
-                        Modifier.padding(bottom = Modifiers.paddingLarge, end = Modifiers.paddingLarge)
-                    ) {
-                        ActionsBar()
-                    }
-                    BottomNavBar()
-                }
-            }
-        }
-    }
+private fun GreetingPreview() {
+    Main()
 }
